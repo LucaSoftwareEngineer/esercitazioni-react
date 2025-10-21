1a1c414b766575b3faa8457d677cbf22ae14e6e9
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-10-21 04:31:15 +0200

    esercizio-2: i dati dinamici

---
+++ src/esercizio-2/Main.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Main = () => {
+
+    const nome = 'Mario';
+    const cognome = 'Rossi';
+
+    return (
+        <div>
+            <p>{nome}</p>
+            <p>{cognome}</p>
+        </div>
+    );
+
+};
+
+export default Main;
