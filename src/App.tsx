9a15b4bcc24dd4942126b787fdb8b2b380e0ced6
LucaSoftwareEngineer <lucapc40@gmail.com>
2025-10-22 14:02:44 +0200

    esercizio-8: componente creato tramite una classe

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Main from "./esercizio-7/Main";
+import Main from "./esercizio-8/Main";
 
 function App() {
   return (
---
+++ src/esercizio-8/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+
+    render() {
+        return <>
+            <b>Componente creato mediante classe</b>
+        </>;
+    }
+
+}
+
+export default Main;
